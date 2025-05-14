8a5617ecc18bdff340e5da2e5f38779c836eecf9
Batanai <batiemc@gmail.com>
2025-05-14 15:10:43 +0200

    auth loading state

--- src/app/providers/AuthProvider.tsx
+++ src/app/providers/AuthProvider.tsx
@@ -1,5 +1,5 @@
-import { View, Text } from 'react-native'
-import React, { Children, createContext, PropsWithChildren, useContext, useState } from 'react'
+import { View, Text, ActivityIndicator } from 'react-native'
+import React, { Children, createContext, PropsWithChildren, useContext, useEffect, useState } from 'react'
 
 const AuthContext = createContext({
     isAuthenticated: false,
@@ -8,8 +8,15 @@ const AuthContext = createContext({
 });
 
 export default function AuthProvider({children}: PropsWithChildren) {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | undefined>(undefined);
   
+  useEffect(() => {
+    const checkAuth = async () => {
+        await new Promise(resolve => setTimeout(resolve, 2000));    
+        setIsAuthenticated(true);
+    } 
+    checkAuth();
+  }, []);
   const signIn = () => {
     setIsAuthenticated(true)
   }
@@ -17,6 +24,13 @@ export default function AuthProvider({children}: PropsWithChildren) {
   const signOut = () => {
     setIsAuthenticated(false)
   }
+
+  if (isAuthenticated === undefined) {
+    return <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size='large' color={'#5343fd'} />
+    </View>
+  }
+  
   return (
     <AuthContext.Provider value={{isAuthenticated, signIn, signOut}}>
       {children}
